80d4623076acae0f02a2b54d2977bb2f3e135060
Ruam Becker <becker.ruam@gmail.com>
2023-01-10 12:57:30 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -100,4 +103,47 @@ public class SellerDaoJDBC implements SellerDao {
 		return null;
 	}
 
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			//guardara qualquer departamento que for instanciado
+			Map<Integer, Department> map = new HashMap<>();
+			
+			//como pode ter mais de um usuario por departamento, fazer um bloco while
+			while(rs.next()) {
+				//verifica se ja possui o departamento instanciado
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				//caso ele não exista sera instanciado
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				Seller obj = instantiateSeller (rs, dep);
+				list.add(obj);
+				
+			}
+			return list;
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			//fechamento da "pesquisa"
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}		
+	}
 }
