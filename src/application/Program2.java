62b3e9540fc2ef41c3e0e6c48bc98bdbf79b192e
Ruam Becker <becker.ruam@gmail.com>
2023-01-17 13:07:05 -0300

    department insert

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -17,15 +17,23 @@ public class Program2 {
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("==== TEST 1: seller findById ====");
+		System.out.println("==== TEST 1: department findById ====");
 		Department dep = departmentDao.findById(1);
 		System.out.println(dep);
 		
-		System.out.println("\n==== TEST 2: seller findAll ====");
+		System.out.println("\n==== TEST 2: department findAll ====");
 		List<Department> list = departmentDao.finAll();
 		for (Department obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n==== TEST 2: department insert ====");
+		Department newDepartment = new Department(null, "Teste");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
+		
+		
+		
 		
 		
 		sc.close();
