3d5c378ce711d6a78cca7925ba4ef43f7c827d0e
Ruam Becker <becker.ruam@gmail.com>
2023-01-18 12:33:52 -0300

    department delete

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -38,6 +38,11 @@ public class Program2 {
 		departmentDao.update(dep);
 		System.out.println("Update completed");
 		
+		System.out.println("\n==== TEST 5: department delete ====");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed!");
 
 		sc.close();
 	}
